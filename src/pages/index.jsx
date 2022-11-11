1a8502042312c9967d969097b95819dd373c7ebd
djamelabbou <djamelabbou@outlook.com>
2022-11-11 17:06:42 +0100

    finished signIn part

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,86 @@
 //import { getUsers } from "@/api/users";
 //import { jsonify } from "@/modules/db";
 import Container from "@/ui/container";
-import {useState} from 'react'
+import { useState } from "react";
 
 export default function HomePage({ users = [] }) {
-  const [username, setUsername] = useState(undefined)
-  const [password, setPassword] = useState(undefined)
-
-  const user = {username, password}
   return (
     <Container className="mt-16 text-white text-2xl flex flex-col gap-8">
-      <div className="flex flex-col gap-5">
-        <div className="">
-          <label className="block text-md relative left-3  text-sky-600">Username</label>
-          <input className="w-1/4 rounded-lg" type="text" value={username} onChange={e => setUsername(e.target.value)}/>
-        </div>
-        <div>
-          <label className="block relative left-3  text-sky-600">Password</label>
-          <input className="w-1/4 rounded-lg" type="text" value={password} onChange={e => setPassword(e.target.value)}/>
-        </div> 
-        <div className="flex items-center justify-center border-2 border-red-500 rounded-lg p-4 w-40" 
-        onClick={() => {}}
-        >Sign Up </div>
-      </div>
+      <SignUp />
+      <SignIn />
     </Container>
+  );
+}
+
+function SignIn() {
+  const [username, setUsername] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
+
+  const credentials = { username, password };
+  return (
+    <div className="flex flex-col gap-5">
+      <div>
+        <label className="block">Username</label>
+        <input
+          className="text-gray-900 rounded-lg w-1/4"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label className="block">Password</label>
+        <input
+          className="text-gray-900 rounded-lg w-1/4"
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div
+        className="border-2 border-red-500 w-40 flex items-center justify-center rounded-lg p-4"
+        onClick={() => {}}
+      >
+        Sign in
+      </div>
+    </div>
+  );
+}
+
+function SignUp({}) {
+  const [username, setUsername] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
+
+  const user = { username, password };
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="">
+        <label className="block text-md relative left-3  text-sky-600">
+          Username
+        </label>
+        <input
+          className="px-2 text-gray-900 w-1/4 rounded-lg"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label className="block relative left-3  text-sky-600">Password</label>
+        <input
+          className="px-2 text-gray-900 w-1/4 rounded-lg"
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div
+        className="flex items-center justify-center border-2 border-red-500 rounded-lg p-4 w-40"
+        onClick={() => {}}
+      >
+        Sign Up{" "}
+      </div>
+    </div>
   );
 }
 
